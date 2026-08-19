60c410aa13a6b676532e9a8ad391233461d0fb50
nagibatorz <zelenkovh@gmail.com>
2026-08-19 11:19:11 -0700

    Time: 26 ms (14.43%) | Memory: 47.7 MB (75.04%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        if(piles.length > h) return -1; // check if not guaranteed
+        int max = 0;
+        for(int num : piles){
+            max = Math.max(num, max);
+        }
+        int l = 1, r = max;
+        int min = Integer.MAX_VALUE;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            //Since h can be up to and the number of piles can be large we can overflow so we use long
+            int hoursTotal = 0;
+
+            for(int i = 0; i < piles.length; i++){
+                // If the pile has less than k bananas, finish eating but not eat from another pile in the same hour
+                hoursTotal += Math.ceil((piles[i] + mid - 1) / mid); 
+            }
+            if(hoursTotal <= h){
+                min = Math.min(mid, min);
+                r = mid - 1;
+            } else{
+                l = mid + 1;
+            }
+        }
+        return min;
+    }
+}
