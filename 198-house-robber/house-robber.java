c45aaab5ffd05fca06f072fbb4cbdc0196112387
nagibatorz <zelenkovh@gmail.com>
2026-08-22 11:08:22 -0700

    Time: 0 ms (100.00%) | Memory: 42.7 MB (75.54%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,12 +1,14 @@
-class Solution {
+// House robber serves as almost like a template DP problem
+class Solution { 
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
         int n = nums.length;
         int[] dp = new int[n + 1];
-        dp[0] = 0;
+        dp[0] = 0; // robbed 0 houses -> 0 money
         dp[1] = nums[0];
         
         for(int i = 1; i < n; i++){
+            //rob curr or skip
             dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
         }
         return dp[n];
