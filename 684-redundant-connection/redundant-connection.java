cba66a25f8a2f8e6e52796d9c6e70529b1fadaa4
nagibatorz <zelenkovh@gmail.com>
2026-08-21 19:55:16 -0700

    Time: 1 ms (91.90%) | Memory: 45.1 MB (38.14%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -5,17 +5,19 @@ class Solution {
             n = Math.max(n, edge[0]);
             n = Math.max(n, edge[1]);
         }
-        UF disjointSet = new UF(n + 1);
+        UF disjointSet = new UF(n + 1); //the problem is 1 indexed
         int[] res = new int[2];
         for(int[] edge : edges){
             if(!disjointSet.union(edge[0], edge[1])){
-                res = edge;
+                res = edge; // return the edge that cause a cycle and was last to iterate on
             }
         }
         return res;
     }
 }
 
+
+// Union Find with path compression optimization for faster look-ups
 class UF{
     int[] parent;
     int[] rank;
@@ -41,14 +43,16 @@ class UF{
     public boolean union(int n1, int n2){
         int pn1 = find(n1);
         int pn2 = find(n2);
-        if(pn1 == pn2){ //already connected
+        if(pn1 == pn2){ //already connected -- cycle detected
             return false;
         }
+        // make sure we are adding to the bigger "root"
         if(rank[pn2] > rank[pn1]){
             int temp = pn2;
             pn2 = pn1;
             pn1 = temp;
         }
+        // union to the bigger root
         parent[pn2] = pn1;
         rank[pn1] += rank[pn2];
         return true; 
