339b1173fddb6b0c04f977a2c199c5889e9f8ae7
nagibatorz <zelenkovh@gmail.com>
2026-08-23 16:34:19 -0700

    Time: 2 ms (86.06%) | Memory: 43.6 MB (77.17%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -21,6 +21,7 @@ class Solution {
 }
 */
 
+//Dynamic Programming memoization approach
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Map<String, Boolean> memo = new HashMap<>();
@@ -29,10 +30,12 @@ class Solution {
 
     private boolean dp(String s, List<String> wordDict, Map<String, Boolean> memo){
         if(s.length() == 0) return true;
+        // check if the answer was previously computed
         if(memo.containsKey(s)) return memo.get(s);
         for(String word : wordDict){
-            if(s.indexOf(word) == 0){
-                String suffix = s.substring(word.length());
+            if(s.indexOf(word) == 0){ //check if word is a prefix of s
+                // recurse on the rest
+                String suffix = s.substring(word.length()); 
                 if(dp(suffix, wordDict, memo)){
                     memo.put(s, true);
                     return true;
