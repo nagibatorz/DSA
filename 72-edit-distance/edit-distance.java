4b57c12cce309cb27652978d43e34d36b962280a
nagibatorz <zelenkovh@gmail.com>
2026-08-23 11:38:29 -0700

    Time: 4 ms (76.00%) | Memory: 47.1 MB (52.94%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -7,18 +7,21 @@ class Solution {
         char[] carr1 = word1.toCharArray();
         char[] carr2 = word2.toCharArray();
 
+        // Transform "" to word 1
         for(int i = 0; i <= n; i++){
             dp[i][0] = i;
         }
+
+        // Transform "" to word 2
         for(int i = 0; i <= m; i++){
             dp[0][i] = i;
         }
 
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                if(carr1[i-1] == carr2[j-1]){
+                if(carr1[i-1] == carr2[j-1]){ // Reuse the previous string - char is the same
                     dp[i][j] = dp[i-1][j-1];
-                } else{
+                } else{ // Pick the most efficient operation
                     dp[i][j] = Math.min(dp[i-1][j-1] + 1, Math.min(dp[i-1][j] + 1, dp[i][j-1] +1));
                 }
             }
