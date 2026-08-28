123a592f9bdd9daf7cbb6ef1b78b4def73143c43
nagibatorz <zelenkovh@gmail.com>
2026-08-28 12:08:09 -0700

    Time: 1 ms (58.49%) | Memory: 47.8 MB (44.44%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,3 +1,4 @@
+// Two pointers
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
@@ -6,9 +7,7 @@ class Solution {
         int trappedWater = 0;
         while(l < r){
 
-            
-            
-            
+            // We need to find the bottleneck side -> find how much water will be trapped
             if(height[l] > height[r]){
                 rightMax = Math.max(rightMax, height[r]);
                 int water = rightMax - height[r];
