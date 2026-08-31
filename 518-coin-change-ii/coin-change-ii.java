3f49686da999282dc02f0eba724260597c0b1e1a
nagibatorz <zelenkovh@gmail.com>
2026-08-31 10:57:18 -0700

    Time: 6 ms (76.52%) | Memory: 42.9 MB (90.77%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,6 +1,7 @@
+// Dynamic Programming Tabulation approach, Optimized Space Complexity
 class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
+        int[] dp = new int[amount + 1]; // we keep one row in memory at a time
         dp[0] = 1; // exactly 1 way to get 0
 
         // Process one coin at a time to get the number of Combinations
@@ -8,6 +9,8 @@ class Solution {
         for(int coin : coins){
             for(int i = coin; i <= amount; i++){
                 if(i - coin >= 0){
+                    // dp[i] - check how many ways there are to compute i;
+                    // if i-coin is valid -> we add however many ways there are to compute i-coin
                     dp[i] = dp[i] + dp[i-coin];
                 }
             }
