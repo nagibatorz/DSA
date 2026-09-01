88f6239eebacdcd5f4fb2d0b45480e838f1a786c
nagibatorz <zelenkovh@gmail.com>
2026-08-31 20:04:40 -0700

    Time: 2 ms (86.75%) | Memory: 49 MB (94.01%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -9,17 +9,25 @@
  * }
  */
 
+// Strategy: find the middle of the list -> reverse second half -> merge first half and reversed second half one-by-one
 class Solution {
     public void reorderList(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
+
+        // find middle + even/odd edge case
         while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
+
+        // first and second half pointers
         ListNode first = head;
         ListNode second = slow.next;
+        //cut off second half from first half
         slow.next = null;
+
+        // reverse second half
         ListNode curr = second;
         ListNode prev = null;
         while(curr != null){
@@ -29,11 +37,19 @@ class Solution {
             curr = temp;
         }
         second = prev;
+
+
         while(first != null && second != null){
+            // save references
             ListNode temp1 = first.next;
             ListNode temp2 = second.next;
+
+            //first points to second
             first.next = second;
+            //second points to the rest of first 
             second.next = temp1;
+
+            //move pointers
             first = temp1;
             second = temp2;
         }
