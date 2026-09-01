7fc8fd799076c28d9b24bcefa6d357dc9d579910
nagibatorz <zelenkovh@gmail.com>
2026-09-01 10:33:05 -0700

    Time: 7 ms (89.07%) | Memory: 45.9 MB (95.84%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,20 +1,28 @@
+// Approach: Dynamic Sliding Winodw + Character Frequency array
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] freq = new int[26];
+        int[] freq = new int[26]; // only uppercase English letters
         char[] carr = s.toCharArray();
+
         int l = 0;
-        int maxF = 0;
+        int maxF = 0; //max frequency variable, only updated at increment
         int res = 0;
+
         for(int r = 0; r < carr.length; r++){
-            int idx = carr[r] - 'A';
+            // increment character frequncy
+            int idx = carr[r] - 'A'; //index of the char in the freq array
             int count = freq[idx] + 1;
             freq[idx]++;
             maxF = Math.max(count, maxF);
 
+            // check if the substring is still valid
+            // aka "do we have enough chars that are the same to maximize result"
             if((r - l + 1) - maxF > k){
+                // decrement frequency and shrink current window
                 freq[carr[l] - 'A']--;
                 l++;
             } else{
+                // if the window is still valid -> update max
                 res = Math.max(res, (r - l + 1));
             }
         }
