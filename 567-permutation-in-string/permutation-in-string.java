0ecf65b7e8e3a7b49adaf2a75f0654329c7f7c5c
nagibatorz <zelenkovh@gmail.com>
2026-09-03 10:01:59 -0700

    Time: 5 ms (97.42%) | Memory: 44.5 MB (30.73%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,37 +1,42 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()) return false;
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-        char[] carr1 = s1.toCharArray();
-        char[] carr2 = s2.toCharArray();
-        for(int i = 0; i < carr1.length; i++){
-            freq1[carr1[i] - 'a']++;
-            freq2[carr2[i] - 'a']++;
+        if (s1.length() > s2.length()) return false;
+        
+        int[] freq = new int[26];
+        // 1. Populate the frequency array for s1
+        for (char c : s1.toCharArray()) {
+            freq[c - 'a']++;
         }
-        int matches = 0;
-        for(int i = 0; i < 26; i++){
-            if(freq1[i] == freq2[i]) matches++;
-        }
+        
+        int required = s1.length(); // How many total valid characters we need
+        char[] s2Chars = s2.toCharArray();
         int l = 0;
-        for(int i = carr1.length; i < carr2.length; i++){
-            if(matches == 26) return true;
-            int r = carr2[i] - 'a';
-            freq2[r]++;
-            if(freq1[r] == freq2[r]){
-                matches++;
-            } else if(freq1[r] + 1 == freq2[r]){
-                matches--;
+        
+        for (int r = 0; r < s2Chars.length; r++) {
+            // If the character at 'r' is something we need, decrease our required count
+            if (freq[s2Chars[r] - 'a'] > 0) {
+                required--;
             }
-            int left = carr2[l] - 'a';
-            freq2[left]--;
-            if(freq1[left] - 1 == freq2[left]){
-                matches--;
-            } else if(freq1[left] == freq2[left]){
-                matches++;
+            // Always decrease the frequency in our map
+            freq[s2Chars[r] - 'a']--;
+            
+            // If our window gets too big, shrink it from the left
+            if (r - l + 1 > s1.length()) {
+                // If the character we are kicking out was one we needed, increase required
+                if (freq[s2Chars[l] - 'a'] >= 0) {
+                    required++;
+                }
+                // Always add the character back to the frequency map
+                freq[s2Chars[l] - 'a']++;
+                l++;
             }
-            l++;
+            
+            // If we found all required characters, we have a match!
+            if (required == 0) {
+                return true;
+            }
         }
-        return matches == 26;
+        
+        return false;
     }
 }
