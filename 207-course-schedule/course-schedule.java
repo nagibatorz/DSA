16a3e2c70c5ace1e9c66f0dd01a7b368b17f106b
nagibatorz <zelenkovh@gmail.com>
2026-09-02 17:04:54 -0700

    Time: 7 ms (56.53%) | Memory: 46.6 MB (97.30%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,40 +1,33 @@
 class Solution {
-    private Map<Integer, List<Integer>> map = new HashMap<>();
-    private Set<Integer> vis = new HashSet<>();
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    Set<Integer> vis = new HashSet<>();
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(prerequisites.length == 0){
-            return true;
-        }
         for(int i = 0; i < numCourses; i++){
             map.put(i, new ArrayList<>());
         }
-        for(int[] curr : prerequisites){
-            map.get(curr[0]).add(curr[1]);
+        for(int[] pre : prerequisites){
+            map.get(pre[0]).add(pre[1]);
         }
 
         for(int c = 0; c < numCourses; c++){
-            if(!dfs(c)){
+            if(hasCycle(c)){
                 return false;
             }
         }
         return true;
     }
 
-    private boolean dfs(int crs){
-        if(vis.contains(crs)){
-            return false;
-        }
-        if(map.get(crs).isEmpty()){
-            return true;
-        }
-        vis.add(crs);
-        for(int pre : map.get(crs)){
-            if(!dfs(pre)){
-                return false;
+    private boolean hasCycle(int course){
+        if(vis.contains(course)) return true;
+        if(map.get(course).isEmpty()) return false;
+        vis.add(course);
+        for(int pre : map.get(course)){
+            if(hasCycle(pre)){
+                return true;
             }
         }
-        vis.remove(crs);
-        map.put(crs, new ArrayList<>());
-        return true;
+        vis.remove(course);
+        map.put(course, new ArrayList<>());
+        return false;
     }
 }
