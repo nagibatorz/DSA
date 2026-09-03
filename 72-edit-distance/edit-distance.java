39c4e82c475b6e3c94bcc9c931e2b1b71edb0287
nagibatorz <zelenkovh@gmail.com>
2026-09-02 17:38:07 -0700

    Time: 3 ms (98.24%) | Memory: 46.3 MB (94.90%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -4,29 +4,27 @@ class Solution {
         if(word1.equals(word2)) return 0;
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n+1][m+1];
+        int[] dp = new int[m+1];
         char[] carr1 = word1.toCharArray();
         char[] carr2 = word2.toCharArray();
 
-        // Transform "" to word 1
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = i;
-        }
-
         // Transform "" to word 2
         for(int i = 0; i <= m; i++){
-            dp[0][i] = i;
+            dp[i] = i;
         }
 
         for(int i = 1; i <= n; i++){
+            int[] newDp = new int[m+1];
+            newDp[0] = i;
             for(int j = 1; j <= m; j++){
                 if(carr1[i-1] == carr2[j-1]){ // Reuse the previous string - char is the same
-                    dp[i][j] = dp[i-1][j-1];
+                    newDp[j] = dp[j-1];
                 } else{ // Pick the most efficient operation
-                    dp[i][j] = 1+ Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                    newDp[j] = 1 + Math.min(dp[j-1], Math.min(dp[j], newDp[j-1]));
                 }
             }
+            dp = newDp;
         }
-        return dp[n][m];
+        return dp[m];
     }
 }
