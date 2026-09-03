2c60a58f9ea192301a634f18ce5c590360f350d2
nagibatorz <zelenkovh@gmail.com>
2026-09-02 17:08:27 -0700

    Time: 7 ms (56.53%) | Memory: 47.5 MB (12.30%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,14 +1,19 @@
+// DFS cycle detection approach
+// if graph has a cycle than it is impossible to take all of the courses
 class Solution {
-    Map<Integer, List<Integer>> map = new HashMap<>();
-    Set<Integer> vis = new HashSet<>();
+    Map<Integer, List<Integer>> map = new HashMap<>(); //adjacency map
+    Set<Integer> vis = new HashSet<>(); // set of currently visiting nodes
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // populate map with empty entries
         for(int i = 0; i < numCourses; i++){
             map.put(i, new ArrayList<>());
         }
+        // populate map with prereqs
         for(int[] pre : prerequisites){
             map.get(pre[0]).add(pre[1]);
         }
 
+        // traverse through the courses and look for cycles
         for(int c = 0; c < numCourses; c++){
             if(hasCycle(c)){
                 return false;
@@ -18,15 +23,20 @@ class Solution {
     }
 
     private boolean hasCycle(int course){
+        //cycle detected
         if(vis.contains(course)) return true;
+        //already traversed the node
         if(map.get(course).isEmpty()) return false;
         vis.add(course);
+        //scan the neighbors if thsie course
         for(int pre : map.get(course)){
             if(hasCycle(pre)){
                 return true;
             }
         }
+        //remove the course from currently visiting because we traversed its neighbors
         vis.remove(course);
+        //indicate the course doesn't have anymore neighbors to visit
         map.put(course, new ArrayList<>());
         return false;
     }
