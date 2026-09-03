c153c46d202e0d97b185c56d82b6a77f228fe592
nagibatorz <zelenkovh@gmail.com>
2026-09-03 09:59:37 -0700

    Time: 5 ms (97.42%) | Memory: 44.1 MB (47.74%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,32 +1,37 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length() > s2.length()) return false;
-        int[] freq = new int[26];
-        char[] carr = s1.toCharArray();
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        char[] carr1 = s1.toCharArray();
         char[] carr2 = s2.toCharArray();
-        for(char c : carr){
-            freq[c - 'a']++;
+        for(int i = 0; i < carr1.length; i++){
+            freq1[carr1[i] - 'a']++;
+            freq2[carr2[i] - 'a']++;
+        }
+        int matches = 0;
+        for(int i = 0; i < 26; i++){
+            if(freq1[i] == freq2[i]) matches++;
         }
         int l = 0;
-        for(int i = 0; i < s1.length(); i++){
-            freq[carr2[i] - 'a']--;
-        }
-        for(int i = s1.length(); i < carr2.length; i++){
-            if(foundAll(freq)) return true;
-            freq[carr2[i] - 'a']--;
-            freq[carr2[l] - 'a']++;
+        for(int i = carr1.length; i < carr2.length; i++){
+            if(matches == 26) return true;
+            int r = carr2[i] - 'a';
+            freq2[r]++;
+            if(freq1[r] == freq2[r]){
+                matches++;
+            } else if(freq1[r] + 1 == freq2[r]){
+                matches--;
+            }
+            int left = carr2[l] - 'a';
+            freq2[left]--;
+            if(freq1[left] - 1 == freq2[left]){
+                matches--;
+            } else if(freq1[left] == freq2[left]){
+                matches++;
+            }
             l++;
         }
-        return foundAll(freq);
+        return matches == 26;
     }
-
-    private boolean foundAll(int[] freq){
-        for(int a : freq){
-            if(a != 0){
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
