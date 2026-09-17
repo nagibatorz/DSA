40cab4457cc8724f7f4d92c70f5a37aa1d8db8ef
nagibatorz <zelenkovh@gmail.com>
2026-09-17 11:11:52 -0700

    Time: 102 ms (9.74%) | Memory: 46.8 MB (6.13%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,13 +1,14 @@
+// Bactracking DFS
 class Solution {
     public int getMaximumGold(int[][] grid) {
         if(grid.length == 0) return 0;
         int n = grid.length, m = grid[0].length;
 
         boolean[][] vis = new boolean[n][m];
-        // boolean[][] done = new boolean[n][m];
         int max = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
+                //start exploring from a cell that has gold
                 if(grid[i][j] > 0){
                     max = Math.max(max, dfs(i, j, n, m, grid, vis));
                 }
@@ -22,6 +23,8 @@ class Solution {
             return 0;
         }
         vis[i][j] = true;
+
+        //find the most profitable directions for the current cell
         int[] directions = new int[4];
         directions[0] = dfs(i, j + 1, n, m, grid, vis);
         directions[1] = dfs(i, j - 1, n, m, grid, vis);
@@ -32,6 +35,8 @@ class Solution {
         for(int k = 0; k < 4; k++){
             max = Math.max(max, directions[k]);
         }
+
+        //add the current cell's gold to the most profitable amount
         return max + grid[i][j];
     }
 }
