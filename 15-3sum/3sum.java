47a270ea0e1b2273d36108ebfcc2af7f70fa5c44
nagibatorz <zelenkovh@gmail.com>
2026-09-17 10:06:27 -0700

    Time: 27 ms (98.77%) | Memory: 59.2 MB (59.83%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,38 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       Arrays.sort(nums);
-       List<List<Integer>> res = new ArrayList<>();
-       int i = 0;
        int n = nums.length;
-       while(i < n){
-            if(nums[i] > 0) break;
-           
-            int l = i + 1, r = n - 1;
-            while(l < r){
-                int sum = nums[i] + nums[l] + nums[r];
-                if(sum == 0){
-                    List<Integer> lst = new ArrayList<>();
-                    lst.add(nums[i]);
-                    lst.add(nums[l]);
-                    lst.add(nums[r]);
-                    res.add(lst);
-                    r--;
-                    l++;
-
-                    while (l < r && nums[l] == nums[l - 1]) l++;
-                    while (l < r && nums[r] == nums[r + 1]) r--;
-                } else if(sum > 0){
-                    r--;
-                } else{
-                    l++;
-                }
+       Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+       for(int i = 0; i < n; i++){
+        if(nums[i] > 0) break; //exit early
+        if(i > 0 && nums[i] == nums[i-1]){ // skip duplicates
+            continue;
+        }
+        int l = i + 1, r = n - 1;
+        while(l < r){ //not binary search
+            int sum = nums[i] + nums[l] + nums[r];
+            if(sum > 0){
+                r--;
+            } else if(sum < 0){
+                l++;
+            } else{
+                res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                l++;
+                r--;
+                //keep incrementing until numbers are distinct
+                while(l < r && nums[l] == nums[l - 1]) l++;
+                while(l < r && nums[l] == nums[r + 1]) r--;
             }
-            i++;
-            while (i < n - 2 && nums[i] == nums[i - 1]) {
-                i++;
-            }
+        }
        }
-
        return res;
     }
 }
