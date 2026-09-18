8d2c7e369d93f6ccb1543389dc17d9c9c6e0302b
nagibatorz <zelenkovh@gmail.com>
2026-09-17 18:55:37 -0700

    Time: 0 ms (100.00%) | Memory: 43.6 MB (46.05%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,19 +1,28 @@
+//Optiimized DP Tabulation
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n = obstacleGrid.length, m = obstacleGrid[0].length;
         int[] paths = new int[m];
+
         if(obstacleGrid[n-1][m-1] == 1) return 0;
+
+        //base case for DP
         paths[m-1] = 1;
+
         for(int i = n - 1; i >= 0; i--){
             for(int j = m - 1; j >= 0; j--){
+
+                //no path if obstacle is in the cell
                 if(obstacleGrid[i][j] == 1){
                     paths[j] = 0;
                 }
+                // bounds -> calculate possible paths
                 else if((j + 1) < m){
                     paths[j] = paths[j] + paths[j+1];
                 }
             }
         }
+        
         return paths[0];
     }
 }
