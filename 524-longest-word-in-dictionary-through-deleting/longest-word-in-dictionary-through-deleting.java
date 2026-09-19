c10eaf0021a7f3d3e8a209c7d17711513330c0df
nagibatorz <zelenkovh@gmail.com>
2026-09-19 16:30:27 -0700

    Time: 16 ms (79.18%) | Memory: 47.7 MB (59.65%) - LeetSync

--- 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.java
+++ 524-longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.java
@@ -1,9 +1,12 @@
 class Solution {
     public String findLongestWord(String s, List<String> dictionary) {
         String res = "";
+        int n = s.length();
         for(String str : dictionary){
-            if(valid(str, s)){
-                if(res.length() < str.length() || (res.length() == str.length() && res.compareTo(str) >= 1)){
+            if(str.length() > n) continue;
+            if(valid(str, s, n)){
+                if(res.length() < str.length() 
+                || (res.length() == str.length() && res.compareTo(str) >= 1)){ // avoid sorting by checking whether new potential answer is lexicographically smaller
                     res = str;
                 }
             }
@@ -11,9 +14,10 @@ class Solution {
         return res;
     }
 
-    private boolean valid(String str, String s){
+    // check if string in dictionary can be built
+    private boolean valid(String str, String s, int n){
         int i = 0, j = 0;
-        while(i < s.length() && j < str.length()){
+        while(i < n && j < str.length()){
             if(str.charAt(j) == s.charAt(i)) j++;
             i++;
         }
