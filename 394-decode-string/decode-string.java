ed22c2d23facd42a40f0449ee97add9f8623446d
nagibatorz <zelenkovh@gmail.com>
2026-09-18 17:50:36 -0700

    Time: 1 ms (83.43%) | Memory: 42.8 MB (74.15%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,25 +1,26 @@
 class Solution {
     public String decodeString(String s) {
         Deque<Integer> counts = new ArrayDeque<>();
-        Deque<StringBuilder> results = new ArrayDeque<>();
+
+        Deque<StringBuilder> results = new ArrayDeque<>(); //stacks, queues, etc. are implemented using generics so storing an object works
 
         int idx = 0;
-        StringBuilder res = new StringBuilder();
+        StringBuilder res = new StringBuilder(); //current string 
 
         while(idx < s.length()){
             char c = s.charAt(idx);
-            if(Character.isDigit(c)){
+            if(Character.isDigit(c)){ //get the entire number of repetitions
                 int count = 0;
                 while(Character.isDigit(s.charAt(idx))){
                     count = count * 10 + (s.charAt(idx) - '0');
                     idx++;
                 }
                 counts.push(count);
-            } else if(c == '['){
+            } else if(c == '['){ //remember current string and build a new one
                 results.push(res);
                 res = new StringBuilder();
                 idx++;
-            } else if(c == ']'){
+            } else if(c == ']'){ //repeat current string 
                 StringBuilder sb = new StringBuilder(results.pop());
                 int count = counts.pop();
                 for(int i = 0; i < count; i++){
@@ -27,7 +28,7 @@ class Solution {
                 }
                 res = sb;
                 idx++;
-            } else{
+            } else{ // build current string 
                 res.append(c);
                 idx++;
             }
