3befc70a805c452f42e3ea021939bf41122c9c5e
nagibatorz <zelenkovh@gmail.com>
2026-09-22 12:47:18 -0700

    Time: 1 ms (96.25%) | Memory: 47 MB (22.17%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,6 +14,8 @@
  * }
  */
 
+
+// BFS 
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         if(root == null) return new ArrayList<>();
@@ -22,13 +24,20 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         while(!q.isEmpty()){
             int n = q.size();
+
+            // pre alocating size of the ArrayList speeds up benchmarking
             List<Integer> level = new ArrayList<>(n);
+
             for(int i = 0; i < n; i++){
                 TreeNode node = q.poll();
                 level.add(node.val);
+
+                //null checks before adding to q
                 if(node.left != null) q.offer(node.left);
                 if(node.right != null) q.offer(node.right);
             }
+
+            //no need to check if level is empty because if we entered the while then we are guaranteed to have nodes at that level
             res.add(level);
         }
         return res;
