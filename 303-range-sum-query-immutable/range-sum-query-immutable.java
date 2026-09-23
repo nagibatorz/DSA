8d21737016969626158c95829f5fefae2e9c7e19
nagibatorz <zelenkovh@gmail.com>
2026-09-23 12:08:08 -0700

    Time: 9 ms (25.36%) | Memory: 47.6 MB (62.39%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,9 +1,9 @@
+//Store prefix sums
 class NumArray {
 
     private int[] pre;
     public NumArray(int[] nums) {
         this.pre = new int[nums.length+1];
-        pre[0] = 0;
         for(int i = 0; i < nums.length; i++){
             pre[i+1] = pre[i] + nums[i];
         }
